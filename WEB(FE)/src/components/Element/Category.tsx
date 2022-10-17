629c1f15f631ad518a8145ba15ffbae3e82aa1d5
Yongjun Park <yopark@student.42seoul.kr>
2022-10-18 01:58:23 +0900

    Fix: /signup/more으로 페이지 이동하지 않도록

--- WEB(FE)/src/components/Element/Category.tsx
+++ WEB(FE)/src/components/Element/Category.tsx
@@ -1,24 +1,23 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 interface CategoryProps {
-  label: string
+  label: string;
 }
 
 export const Category = ({ label }: CategoryProps) => {
-
   let [checked, setClicked] = useState(false);
 
+  const toggleClicked = () => {
+    setClicked((checked) => !checked);
+  };
+
   return (
-    <button className={`w-32 h-32 rounded-2xl ${!checked ? 'bg-white-200' : 'bg-orange'}`}
-
-      onClick={() => {
-        setClicked(checked => !checked)
-      }}>
-
-      <p className={`text-center text-2xl font-semibold ${!checked ? 'text-gray-400' : 
-      'text-white-200'}`}>{label}</p>
+    <button
+      className={`w-32 h-32 rounded-2xl text-center text-xl font-semibold m-1 ${
+        !checked ? 'bg-white-200 text-gray-400' : 'bg-orange text-white-200'
+      }`}
+      onClick={toggleClicked}>
+      {label}
     </button>
-
-
-  )
-}
+  );
+};
