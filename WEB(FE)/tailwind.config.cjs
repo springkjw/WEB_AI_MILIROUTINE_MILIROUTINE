ab1fa857be08f03e60e16aa98e914814e8001308
Yongjun Park <yopark@student.42seoul.kr>
2022-10-23 02:22:02 +0900

    Feat: /user/setting 전체 페이지 제작

--- WEB(FE)/tailwind.config.cjs
+++ WEB(FE)/tailwind.config.cjs
@@ -1,30 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        colors: {
-            orange: '#F07644',
-            black: '#262626',
-            white: {
-                100: '#F5F5F5',
-                200: '#F2F2F2',
-            },
-            gray: {
-                300: '#D9D9D9',
-                400: '#AAAAAA',
-                500: '#777777',
-            },
-            blue: '#0066FF',
-            green : '#5EC14E',
-        },
-        extend: {
-          width: {
-            '420' : '420px',
-          },
-          padding: {
-            '515' : '515px',
-          },
-        },
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      orange: '#F07644',
+      red: '#F23D3D',
+      yellow: '#F5B954',
+      black: '#262626',
+      white: {
+        100: '#F5F5F5',
+        200: '#F2F2F2',
+      },
+      gray: {
+        300: '#D9D9D9',
+        400: '#AAAAAA',
+        500: '#777777',
+      },
+      blue: '#0066FF',
+      green: '#5EC14E',
     },
-    plugins: [],
+    extend: {
+      width: {
+        420: '420px',
+      },
+      padding: {
+        515: '515px',
+      },
+    },
+  },
+  plugins: [],
 };
